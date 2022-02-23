62c38193fb06734f2b8ab3d0fa818279a2aa3039
Jasonchu94 <Jasonchu7272@gmail.com>
2022-02-23 15:12:53 -0800

    add graph functionality for set time range

--- Stock_Stock_Go/src/index.js
+++ Stock_Stock_Go/src/index.js
@@ -1,5 +1,4 @@
 import StockApp from "./scripts/stockApp";
-import Graph from "./scripts/graph";
 
 document.addEventListener("DOMContentLoaded", () => {
     const stockApp = new StockApp();
--- Stock_Stock_Go/src/scripts/stockApp.js
+++ Stock_Stock_Go/src/scripts/stockApp.js
@@ -1,95 +1,72 @@
-import Graph from './graph'
-// require('dotenv').config();
 class StockApp{
 
     constructor(){
         this.ctx = document.getElementById("myChart").getContext('2d');
-        this.allYears = [];
-
-
+        this.ctx.font = '50px Arial'
+        this.ctx.fillText("Select a start date and end date", 600, 300)
+        this.ctx.fillText("Enter a 4 letter ticker symbol to generate a graph!", 400, 400)
         this.input = document.getElementById('ticker-search-bar');
         this.input.addEventListener('input', this.addStock);
 
-        this.startYear=document.getElementById('year-range')
-        this.startYear.addEventListener('input', this.setYearRange)
-
-        
-        // this.getStock();
         this.addStock = this.addStock.bind(this);
-        this.setYearRange = this.setYearRange.bind(this);
-        // debugger
-        this.drawGraph();
+       
     }
 
-    setYearRange(e){
-        e.preventDefault();
-        this.yearRange = e.currentTarget.value.split(' ')
-        if (Number(this.yearRange[0]) > Number(this.yearRange[1])){
-            console.log("error")
-        }
-        else{         
-            for(let i = Number(this.yearRange[0]); i<=Number(this.yearRange[1]); i++){
-                this.allYears.push(i)
-            }
-        }
-
-    }
-    addStock(e){
+    addStock(e) {
         e.preventDefault();
         // debugger
-        let input = e.currentTarget.value
-        if (input.length === 4){
+        let input = e.currentTarget.value.toUpperCase();
+        if (input.length === 4) {
             e.currentTarget.value = ""
-            console.log(input.toUpperCase())
+            this.startYear = document.getElementById('start-year').value
+            this.endYear = document.getElementById('end-year').value
+            this.ctx = document.getElementById("myChart").getContext('2d');
+
+            let stockData = fetch(`https://cloud.iexapis.com/stable/stock/${input}/quote?token=pk_b07152883e9d4a61a719dc430195a97b`)
+                .then(response => response.json())
+                .then(data => this.data = data)    
+            debugger
+                const myChart = new Chart(this.ctx, {
+                    type: 'line',
+                    data: {
+                        labels: [this.startYear, this.endYear],
+                        datasets: [{
+                            label: `Stock Prices from ${this.startYear} to ${this.endYear}`,
+                            data: [1, 2],
+                            // backgroundColor: [
+                            //     'rgba(255, 99, 132, 0.2)',
+                            //     'rgba(54, 162, 235, 0.2)',
+                            //     'rgba(255, 206, 86, 0.2)',
+                            //     'rgba(75, 192, 192, 0.2)',
+                            //     'rgba(153, 102, 255, 0.2)',
+                            //     'rgba(255, 159, 64, 0.2)'
+                            // ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            },
+    
+                        }
+                    }
+                });
+            
+          
+
         }
     }
 
-
-    getStock(ticker){
-        // debugger
-        let search = document.getElementById("ticker-search-bar")
-        let button = document.getElementById('ticker-search-button')
-        console.log('hello')
-    }
-
-    drawGraph(){
-        // debugger
-        const myChart = new Chart(this.ctx, {
-            type: 'line',
-            data: {
-                labels: this.allYears,
-                datasets: [{
-                    label: 'Stock Prices over',
-                    data: [1,2,3,4,5,5],
-                    // backgroundColor: [
-                    //     'rgba(255, 99, 132, 0.2)',
-                    //     'rgba(54, 162, 235, 0.2)',
-                    //     'rgba(255, 206, 86, 0.2)',
-                    //     'rgba(75, 192, 192, 0.2)',
-                    //     'rgba(153, 102, 255, 0.2)',
-                    //     'rgba(255, 159, 64, 0.2)'
-                    // ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    },
-                
-                }
-            }
-        });
-    }
 }
 
 export default StockApp;
