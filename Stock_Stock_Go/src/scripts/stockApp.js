777c4e680174d8822eeee6a36505c159ffa00681
Jasonchu94 <Jasonchu7272@gmail.com>
2022-02-23 16:07:09 -0800

    fix css for graph space

--- Stock_Stock_Go/src/scripts/stockApp.js
+++ Stock_Stock_Go/src/scripts/stockApp.js
@@ -2,13 +2,16 @@ class StockApp{
 
     constructor(){
         this.ctx = document.getElementById("myChart").getContext('2d');
+        // debugger
         this.ctx.font = '50px Arial'
-        this.ctx.fillText("Select a start date and end date", 600, 300)
-        this.ctx.fillText("Enter a 4 letter ticker symbol to generate a graph!", 400, 400)
+        this.ctx.fillText("Select a start date and end date",100,300)
+        this.ctx.fillText("Enter a 4 letter ticker symbol to generate a graph!", 100, 400)
         this.input = document.getElementById('ticker-search-bar');
-        this.input.addEventListener('input', this.addStock);
+        this.input.addEventListener('input', this.addStock.bind(this));
 
         this.addStock = this.addStock.bind(this);
+        this.noStockFound = this.noStockFound.bind(this);
+        this.graphStock= this.graphStock.bind(this);
        
     }
 
@@ -20,53 +23,31 @@ class StockApp{
             e.currentTarget.value = ""
             this.startYear = document.getElementById('start-year').value
             this.endYear = document.getElementById('end-year').value
+            this.canvas = document.getElementById('myChart')
             this.ctx = document.getElementById("myChart").getContext('2d');
 
-            let stockData = fetch(`https://cloud.iexapis.com/stable/stock/${input}/quote?token=pk_b07152883e9d4a61a719dc430195a97b`)
+            fetch(`https://cloud.iexapis.com/stable/stock/${input}/quote?token=pk_b07152883e9d4a61a719dc430195a97b`)
                 .then(response => response.json())
-                .then(data => this.data = data)    
-            debugger
-                const myChart = new Chart(this.ctx, {
-                    type: 'line',
-                    data: {
-                        labels: [this.startYear, this.endYear],
-                        datasets: [{
-                            label: `Stock Prices from ${this.startYear} to ${this.endYear}`,
-                            data: [1, 2],
-                            // backgroundColor: [
-                            //     'rgba(255, 99, 132, 0.2)',
-                            //     'rgba(54, 162, 235, 0.2)',
-                            //     'rgba(255, 206, 86, 0.2)',
-                            //     'rgba(75, 192, 192, 0.2)',
-                            //     'rgba(153, 102, 255, 0.2)',
-                            //     'rgba(255, 159, 64, 0.2)'
-                            // ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)'
-                            ],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            y: {
-                                beginAtZero: true
-                            },
-    
-                        }
-                    }
-                });
-            
-          
-
+                .then(data => this.data = data)
+                // .then(this.graphStock(this.data))    
+                .catch(this.noStockFound())
+               
         }
+        // console.log(this.data)
+    }
+    graphStock(data){
+        debugger
+        if(data === undefined) return null
+        console.log(data)
+        console.log('graph it up')
+    }
+    noStockFound(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.font = '50px Arial'
+        this.ctx.fillText("No ticker found for your entry", 400, 300)
     }
 
 }
 
 export default StockApp;
+
